be613e541d06312788c0bcb489683e53b8f50cc5
Matei Adriel <rafaeladriel11@gmail.com>
2020-01-16 22:56:20 +0200

    refactor: refactored to IO<void> for side effects

--- src/modules/editor/components/Editor.tsx
+++ src/modules/editor/components/Editor.tsx
@@ -1,37 +1,38 @@
 import { useProfunctorState } from '@staltz/use-profunctor-state'
 import { add2, sub2 } from '@thi.ng/vectors'
 import * as Array from 'fp-ts/es6/Array'
-import { flow, constant } from 'fp-ts/es6/function'
-import * as IO from 'fp-ts/es6/IO'
+import { array } from 'fp-ts/es6/Array'
+import { constant, flow, tuple } from 'fp-ts/es6/function'
+import { IO, io } from 'fp-ts/es6/IO'
 import * as Option from 'fp-ts/es6/Option'
 import { pipe } from 'fp-ts/es6/pipeable'
+import * as Reader from 'fp-ts/es6/Reader'
+import * as Record from 'fp-ts/es6/Record'
+import * as State from 'fp-ts/es6/State'
+import { snd } from 'fp-ts/es6/Tuple'
 import { h } from 'preact'
 import { useEffect, useState } from 'preact/hooks'
 import { MouseButtons } from '../../core/constants'
 import { getElementId } from '../../core/lenses/html'
 import { full } from '../../core/styles/full'
-import { identitySetter } from '../../lens/helpers/identitySetter'
 import { resolveEventTarget } from '../helpers/resolveEventTarget'
 import { selectNode } from '../helpers/selectNode'
 import { spawnNode } from '../helpers/spawnNode'
 import { startsWith } from '../helpers/startsWith'
 import { unselectNodes } from '../helpers/unselectNodes'
 import { liftNode } from '../lenses/liftNode'
-import { getNodesArray, setSelectedNodes } from '../lenses/nodesArray'
+import { setSelectedNodes } from '../lenses/nodesArray'
 import { EditorState, vNodeOrd, VNodeState } from '../types/EditorState'
 import { renderNode } from './Node'
-import { snd } from 'fp-ts/es6/Tuple'
-import * as State from 'fp-ts/es6/State'
-import * as Reader from 'fp-ts/es6/Reader'
 
 export const Editor = () => {
-    const { promap, setState, state: editorState } = useProfunctorState<
-        EditorState
-    >({
+    const { state, setState } = useProfunctorState<EditorState>({
         laseZIndex: -1,
         nodes: {}
     })
 
+    // At the start of the program we spawn 2 nodes for testing
+    // TODO remove
     useEffect(() => {
         const spawner = constant(spawnNode())
 
@@ -41,19 +42,25 @@ export const Editor = () => {
             Reader.chain(flow(snd, constant))
         )
 
-        const sideEffect: IO.IO<void> = flow(spawn, setState)
+        const sideEffect: IO<void> = flow(spawn, setState)
 
         sideEffect()
     }, [])
 
-    const { state: nodes } = promap(getNodesArray, identitySetter)
     const [lastMousePosition, setLastMousePosition] = useState<
         Option.Option<number[]>
     >(Option.none)
 
     const handleMouseUp = () => {
-        setLastMousePosition(Option.none)
-        setState(unselectNodes)
+        const sideEffect = pipe(
+            [
+                () => setState(unselectNodes),
+                () => setLastMousePosition(Option.none)
+            ],
+            array.sequence(io)
+        )
+
+        sideEffect()
     }
 
     const handleMouseDown = ({ target, buttons }: MouseEvent) => {
@@ -68,45 +75,66 @@ export const Editor = () => {
 
         // Id of the node clicked
         const id = pipe(
-            target as HTMLElement | null,
+            target,
             Option.fromNullable,
             resolveEventTarget(predicate),
-            Option.map(flow(getElementId, withoutPrefix, Number))
+            Option.map(element => {
+                const id = pipe(element, getElementId, withoutPrefix, Number)
+
+                const stateUpdater = flow(
+                    liftNode(id),
+                    unselectNodes,
+                    selectNode(id)
+                )
+
+                const sideEffect: IO<void> = () => setState(stateUpdater)
+
+                sideEffect()
+            })
         )
-
-        // this modifies the state of the app
-        const update = (id: number) =>
-            flow(liftNode(id), unselectNodes, selectNode(id))
-
-        // this commits the state
-        return pipe(id, Option.map(flow(update, setState)))
     }
 
     const handleMouseMove = ({ clientX, clientY, buttons }: MouseEvent) => {
-        const currentPosition = [clientX, clientY]
-
         if (!(buttons & MouseButtons.left)) {
             return
         }
 
-        pipe(
-            lastMousePosition,
-            Option.map(position => {
-                const delta = sub2([], currentPosition, position) as number[]
+        const currentPosition = [clientX, clientY]
 
-                return setSelectedNodes(
-                    VNodeState.transform.position.set(
-                        old => add2([], delta, old) as number[]
+        const moveIO: IO<void> = () =>
+            pipe(
+                lastMousePosition,
+                Option.map(position => {
+                    const delta = sub2(
+                        [],
+                        currentPosition,
+                        position
+                    ) as number[]
+
+                    return setState(
+                        setSelectedNodes(
+                            VNodeState.transform.position.set(
+                                old => add2([], delta, old) as number[]
+                            )
+                        )
                     )
-                )
-            }),
-            Option.map(setState)
-        )
+                })
+            )
 
-        pipe(currentPosition, Option.some, setLastMousePosition)
+        const currentPositionUpdater: IO<void> = () =>
+            pipe(currentPosition, Option.some, setLastMousePosition)
+
+        const sideEffect = array.sequence(io)([currentPositionUpdater, moveIO])
+
+        sideEffect()
     }
 
-    const children = pipe(nodes, Array.sort(vNodeOrd), Array.map(renderNode))
+    const children = pipe(
+        state.nodes,
+        Record.collect(flow(tuple, snd)),
+        Array.sort(vNodeOrd),
+        Array.map(renderNode)
+    )
 
     return (
         <svg
