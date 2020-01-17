b15625c28423efdb2157cef12035a28eed60af2b
Matei Adriel <rafaeladriel11@gmail.com>
2020-01-17 14:48:34 +0200

    refactor: removed for loop and pubsub from `initGraph`

--- src/modules/dataflow/helpers/initGraph.ts
+++ src/modules/dataflow/helpers/initGraph.ts
@@ -1,36 +1,32 @@
-import { pubsub, sync } from '@thi.ng/rstream'
+import { sync } from '@thi.ng/rstream'
 import * as tx from '@thi.ng/transducers'
 import * as Either from 'fp-ts/es6/Either'
+import { flow, tuple } from 'fp-ts/es6/function'
 import * as IO from 'fp-ts/es6/IO'
 import { pipe } from 'fp-ts/es6/pipeable'
+import * as Record from 'fp-ts/es6/Record'
+import { snd } from 'fp-ts/es6/Tuple'
 import { unknownTypeError } from '../../typeChecking/constants'
 import { getConnectionStart } from '../../typeChecking/helpers/getConnectionStart'
 import { getInputPinLabel } from '../../typeChecking/helpers/getInputPinLabels'
 import { isUnknown } from '../../typeChecking/helpers/isUnknown'
-import { LabelValidationError } from '../../typeChecking/types/Errors'
 import { SVariableInstance } from '../../typeChecking/types/Labels'
 import { SNode } from '../types/SGraph'
 
 export const initGraph = (_module: SNode[]) => {
     for (const node of _module) {
         const streams = node.inputs.map(input => {
-            const connection = input.connection
-
             // validate input
             const type = getInputPinLabel(input, new Set())
 
             if (
                 pipe(
                     type,
-                    Either.filterOrElse(isUnknown, IO.of(unknownTypeError)),
-                    Either.isLeft
-                )
+                    Either.filterOrElse(isUnknown, IO.of(unknownTypeError))
+                ) &&
+                type._tag === 'Left'
             ) {
-                const { left: error } = type as Either.Left<
-                    LabelValidationError
-                >
-
-                throw error
+                throw type.left
             }
 
             // This comment was from a random joke you most probably can't understand
@@ -38,7 +34,7 @@ export const initGraph = (_module: SNode[]) => {
 
             // I didn't to it, it wasn't me!!! See? I am coding!!!
 
-            return getConnectionStart(connection).source
+            return getConnectionStart(input.connection).source
         })
 
         const indexedResults = sync<
@@ -47,24 +43,13 @@ export const initGraph = (_module: SNode[]) => {
         >({
             src: streams
         }).transform(
-            tx.map(Object.values),
-            tx.mapcat(node.transformation),
-            tx.indexed()
+            tx.map(flow(Record.collect(flow(tuple, snd)), node.transformation))
         )
 
-        const splitter = pubsub({
-            topic: (a: [number, SVariableInstance]) => a[0]
-        })
-
-        indexedResults.subscribe(splitter)
-
-        for (let index = 0; index < node.outputs.length; index++) {
-            splitter
-                .subscribeTopic(
-                    index,
-                    tx.map(v => v[1])
-                )
-                .subscribe(node.outputs[index].source)
-        }
+        node.outputs.forEach((node, index) =>
+            indexedResults
+                .transform(tx.map(outputs => outputs[index]))
+                .subscribe(node.source)
+        )
     }
 }
