df141876251c3691dc04acfcc3ec7c156c90bdce
BlueGhostGH <engisoftleaderoff@gmail.com>
2020-02-10 01:24:13 +0200

    feat(api): added an action for fetching sessions

---
+++ packages/api/src/modules/session/actions/getCookie.ts
@@ -0,0 +1,13 @@
+import db from "../../../../db/knex"
+
+import { Session } from "../types/Session"
+
+export const getCookie = async (key: Session["key"]) => {
+    const cookie = await db<Session>("sessions")
+        .where({ key })
+        .select("key", "maxAge", "session")
+        .first()
+
+    if (!cookie) return undefined
+    return cookie.session
+}
