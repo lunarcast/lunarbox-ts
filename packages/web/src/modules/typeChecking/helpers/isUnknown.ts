155bee4c96464f2c8332568ad9413b24fb48b53b
Matei Adriel <rafaeladriel11@gmail.com>
2020-02-09 17:00:33 +0200

    chore(web): deleted old dataflow stuff

--- packages/web/src/modules/typeChecking/helpers/isUnknown.ts
+++
@@ -1,7 +0,0 @@
-import { LabelCode } from '../types/Labels'
-import { isOfLabel } from './isOfLabel'
-
-/**
- * Returns true if the type is known, false otherwise
- */
-export const isUnknown = isOfLabel(LabelCode.void)
